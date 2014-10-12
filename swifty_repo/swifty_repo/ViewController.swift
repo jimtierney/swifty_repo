e3bdc38fc7900c378b0e33b52796da0269c42f5c
Jim Tierney <jim.tierney@apjam.co.uk>
2014-10-12 18:50:50 +0100

    Added IBAction to class file and implemented some basic change action to label

--- swifty_repo/swifty_repo/ViewController.swift
+++ swifty_repo/swifty_repo/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myFirstLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,11 +24,40 @@ class ViewController: UIViewController {
         var myFirstVariable = "My first variable which will be a string in this instance"
         let myFirstConstant = [myFirstVariable:"firstKey"]
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.myFirstLabel.text = myFirstVariable
+        self.myFirstLabel.layer.cornerRadius = 10
+        self.myFirstLabel.layer.masksToBounds = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+       @IBAction func myFirstAction(sender: AnyObject) {
+        
+        if self.myFirstLabel.backgroundColor==UIColor.redColor() {
+            self.myFirstLabel.backgroundColor = UIColor.whiteColor()
+            self.actionButton.setTitle("ACTION", forState: .Normal)
+            self.actionButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+            self.actionButton.backgroundColor = UIColor.blackColor()
+            self.myFirstLabel.text = 
+            return
+        }
+        
+        //When the button is pressed I intend for the label to change colour and the button to change text
+        self.myFirstLabel.backgroundColor = UIColor.redColor()
+        self.actionButton.setTitle("BAM", forState: .Normal)
+        self.actionButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        self.actionButton.backgroundColor = UIColor.redColor()
+        
+    }
+    
+    func thisIsMyFirstMethod(var,NSString){
+        var returnedVariable = "returned string"
+        return returnedVariable
     }
 
 
